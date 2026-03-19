feab7569c36fd5e7636d7d7b41c27559a721abd5
ranafie <ranafie.sirat@gmail.com>
2026-03-19 10:12:33 +0800

    auto update

--- deploy_penguatkuasa.bat
+++ deploy_penguatkuasa.bat
@@ -1,5 +1,5 @@
 @echo off
-title Penguatkuasa System - One Click Deploy
+title Penguatkuasa System - Deploy
 
 echo ===============================
 echo PENGUATKUASA SYSTEM DEPLOY
@@ -9,13 +9,16 @@ cd /d C:\xampp_new\htdocs\penguatkuasa
 
 echo.
 set /p msg=Enter commit message:
+if "%msg%"=="" set msg=auto update
 
 git add .
-git commit -m "%msg%"
-git push
+git commit -m "%msg%" || echo No changes to commit
+
+git push origin main
 
 echo.
 echo ===============================
-echo DEPLOY COMPLETE
+echo DEPLOY COMPLETE (PENGUATKUASA)
 echo ===============================
+
 pause
